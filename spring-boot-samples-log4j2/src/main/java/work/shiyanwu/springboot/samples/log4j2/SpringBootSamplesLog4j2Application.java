f2502008aaa3c4353bcee752035ddc4491655827
291122524@qq.com <shiYanWu199198>
2020-04-28 22:09:19 +0800

    SpringBoot示例项目

---
+++ spring-boot-samples-log4j2/src/main/java/work/shiyanwu/springboot/samples/log4j2/SpringBootSamplesLog4j2Application.java
@@ -0,0 +1,13 @@
+package work.shiyanwu.springboot.samples.log4j2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamplesLog4j2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSamplesLog4j2Application.class, args);
+    }
+
+}
